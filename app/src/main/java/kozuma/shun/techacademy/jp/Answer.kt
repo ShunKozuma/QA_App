6f0e45ffb65d4afbab8a10c60c22aac3502b9c60
ShunKozuma <s-kozuma@i-enter.co.jp>
2019-05-08 15:43:47 +0900

    最初のコミット （課題取り組み前）

---
+++ app/src/main/java/kozuma/shun/techacademy/jp/Answer.kt
@@ -0,0 +1,5 @@
+package kozuma.shun.techacademy.jp
+
+import java.io.Serializable
+
+class Answer(val body: String, val name: String, val uid: String, val answerUid: String) : Serializable
---
+++ app/src/main/java/kozuma/shun/techacademy/jp/Question.kt
@@ -0,0 +1,11 @@
+package kozuma.shun.techacademy.jp
+
+import java.io.Serializable
+
+class Question(val title: String, val body: String, val name: String, val uid: String, val questionUid: String, val genre: Int, bytes: ByteArray, val answers: ArrayList<Answer>): Serializable {
+    val imageBytes: ByteArray
+
+    init {
+        imageBytes = bytes.clone()
+    }
+}
